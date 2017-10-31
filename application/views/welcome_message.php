9a4b2ae9aa63fe0927f27f1d12639dbf8aad4a3b
ThitipongInlom <ingnice007@gmail.com>
2017-10-31 15:33:03 +0700

    แก้ไขการจัดโครงสร้าง

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>Storage | QR</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -15,7 +15,11 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/adminlte/dist/css/AdminLTE.min.css'; ?>">
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/adminlte/plugins/iCheck/square/purple.css'; ?>">
+  <link rel="stylesheet" href="<?php echo base_url().'assets/adminlte/plugins/iCheck/square/blue.css'; ?>">
+  <!-- Alertify -->
+  <link rel="stylesheet" href="<?php echo base_url().'assets/alertify/css/alertify.min.css'; ?>" />
+   <!-- Alertify theme -->
+  <link rel="stylesheet" href="<?php echo base_url().'assets/alertify/css/themes/semantic.min.css'; ?>" />
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,70 +34,35 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="#"><b>Storage</b> QR</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
-
-    <form action="../../index2.html" method="post">
+    <p class="login-box-msg">ลงชื่อเข้าใช้เพื่อเริ่มต้นเซสชันของคุณ</p>
+    <form name="formlogin" id="formlogin">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Username" id="username">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" id="password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="button" class="btn btn-primary btn-block btn-flat" id="login">Sign In
+            <i class="fa fa-refresh fa-spin fa-fw" id="loginicon" style="margin-right: 2px;"></i></button>
         </div>
         <!-- /.col -->
       </div>
-    </form>
-
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
-
-<div class="box box-default box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">ดูตัวอย่าง</h3>
-
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-              </div>
-              <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <button type="button" class="btn btn-block btn-success btn-flat">DEMO</button>
-            </div>
-            <!-- /.box-body -->
-          </div>
+      <form>
   </div>
   <!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
-
 
 <!-- jQuery 3 -->
 <script src="<?php echo base_url().'assets/adminlte/bower_components/jquery/dist/jquery.min.js'; ?>"></script>
@@ -103,11 +72,15 @@
 <script src="<?php echo base_url().'assets/adminlte/plugins/iCheck/icheck.min.js'; ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url().'assets/adminlte/dist/js/adminlte.min.js'; ?>"></script>
+<!-- Alertify -->
+<script src="<?php echo base_url().'assets/alertify/alertify.min.js'; ?>"></script>
+<!-- Login -->
+<script src="<?php echo base_url().'assets/js_modifly/login.js'; ?>"></script>
 <script>
   $(function () {
     $('input').iCheck({
-      checkboxClass: 'icheckbox_square-purple',
-      radioClass: 'iradio_square-purple',
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
       increaseArea: '20%' // optional
     });
   });

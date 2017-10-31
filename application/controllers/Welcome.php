b86b52c83e98fe092e22f6e5103628ffb32cf046
ThitipongInlom <ingnice007@gmail.com>
2017-10-31 15:32:42 +0700

    ตรวจสอบการล็อกอิน

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,8 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Welcome_model');
+	    $this->load->library('session_user');
+		
+	}
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
+
+	}
+
+	public function Login()
+	{
+		$result = $this->Welcome_model->login_model();
+		// if result == 1  --200-- OK
+		if ($result['result'] == '1') {
+			$dataset = $this->session_user->set_session($result['row']);
+			$this->session->set_userdata($dataset);
+			echo '1';
+		}
 	}
 }

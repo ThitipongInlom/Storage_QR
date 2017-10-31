88a55fc5c156d62565d31c630f3eebfe7ce04e8c
ThitipongInlom <ingnice007@gmail.com>
2017-10-31 16:11:37 +0700

    หน้า Home

---
+++ application/views/home.php
@@ -0,0 +1,77 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css'; ?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/bower_components/font-awesome/css/font-awesome.min.css'; ?>">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/bower_components/Ionicons/css/ionicons.min.css'; ?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/dist/css/AdminLTE.min.css'; ?>">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/dist/css/skins/_all-skins.min.css'; ?>">
+   <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url().'/assets/adminlte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'; ?>">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.1.1/css/responsive.dataTables.min.css">
+ 
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body>
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        หน้ารวมสินค้า 
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="<?php echo site_url('Home'); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+      </ol>
+    </section>
+    <!-- Main content -->
+<section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box box-primary ">
+            <div class="box-header with-border">
+              <h3 class="box-title"><b><?php echo $this->session->data_tname.' '.$this->session->data_firstname.' '.$this->session->data_lastname; ?></b></h3>
+            </div>
+          </div>
+        </div>
+      </div>
+       
+       <div class="row">
+          <div class="col-md-12">
+            <?php print_r($this->session->all_userdata()); ?>
+          </div>
+       </div> 
+    </section>
+  <!-- /.content-wrapper -->
+  </div>
+<!-- DataTables -->
+<script src="<?php echo base_url().'/assets/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js'; ?>"></script>
+<script src="<?php echo base_url().'/assets/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js'; ?>"></script>
+<script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js" type="text/javascript" charset="utf-8" ></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url().'/assets/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js'; ?>"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url().'/assets/adminlte/bower_components/fastclick/lib/fastclick.js'; ?>"></script>
+</body>
+</html>

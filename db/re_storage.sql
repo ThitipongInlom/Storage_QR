9f13a5d799d55a6e103438e0ef55bf8686b7b8c9
ThitipongInlom <ingnice007@gmail.com>
2017-11-11 17:22:48 +0700

    อัพเดท Sql
    อัพเดท Sql

--- db/re_storage.sql
+++ db/re_storage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2017 at 05:10 PM
+-- Generation Time: Nov 11, 2017 at 11:22 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -25,14 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(8) NOT NULL,
+  `item_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `item_mac` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `item_imgname` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `item_name`, `item_mac`, `item_imgname`) VALUES
+(5, 'Test', 'Test', 'e4da3b7fbbce2345d7772b0674a318d5.png'),
+(6, '123', '123', '1679091c5a880faf6fb5e6087eb1b2dc.png'),
+(7, '^^', '66', '8f14e45fceea167a5a36dedd4bea2543.png'),
+(8, 'asd', 'ads', 'c9f0f895fb98ab9159f51fd0297e236d.png'),
+(9, '123', '3', '45c48cce2e2d7fbdea1afc51c7c6ad26.png'),
+(10, '123', '32', 'd3d9446802a44259755d38e6d163e820.png'),
+(11, '32', '32', '6512bd43d9caa6e02c990b0a82652dca.png');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(8) NOT NULL,
-  `user_username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `user_username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
@@ -46,6 +72,12 @@ INSERT INTO `user` (`user_id`, `user_username`, `user_password`) VALUES
 --
 
 --
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -54,6 +86,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user`

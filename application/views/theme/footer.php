559fbf63177b170159857c935292780689f993cc
ThitipongInlom <ingnice007@gmail.com>
2017-12-03 19:26:51 +0700

    อัพเดต เวอร์ชัั่น
    -อัพเดตเวอร์ชั่นจาก 0.0.1 เป็น 0.02
    -เพิ่มเครดิต และ Github

--- application/views/theme/footer.php
+++ application/views/theme/footer.php
@@ -1,8 +1,8 @@
   <footer class="main-footer">
     <div class="pull-right">
-      <b>Version</b> 0.0.1
+      <b>Version</b> 0.0.2
     </div>
-    <strong>Copyright &copy; 2017 </strong> 
+    <strong>Copyright &copy; 2017 - <a href="https://github.com/ingnice">Thitipong Inlom</a></strong> 
   </footer>
   </div>
 </body> 
